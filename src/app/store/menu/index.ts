59034e96c9c18697aa558f4f9098d06b2c29a750
snakerten96 <neil.lunn1@gmail.com>
2016-09-21 16:29:52 +1000

    add store menu

---
+++ src/app/store/menu/index.ts
@@ -0,0 +1,11 @@
+import { IMenu, IMenuItem } from './menu.types';
+import { menuReducer } from './menu.reducer';
+import { deimmuitfyMenu, reimmutifyMenu } from './menu.transformers';
+
+export {
+  IMenu,
+  IMenuItem,
+  menuReducer,
+  deimmuitfyMenu,
+  reimmutifyMenu
+};
