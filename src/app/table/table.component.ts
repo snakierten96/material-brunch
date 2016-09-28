25e0a7ddad37a6c4de5dc679bad19442b7ab45ed
snakierten96 <neil.lunn1@gmail.com>
2016-09-28 03:54:59 +0000

    Refactor material bundle Add theming for new bundle Refactor layout

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -10,10 +10,12 @@ import { IParty } from '../store';
 
 @Component({
   selector: 'tb-table',
-  templateUrl: './table.component.html'
+  templateUrl: './table.component.html',
+  styleUrls: ['./table.component.css']
 })
 export class TableComponent {
   @Input() table: any;
   @Input() parties: Observable<IParty>;
   @Output() seatParty: EventEmitter<any> = new EventEmitter<any>();
+  partyId: number = -1;
 }
